8e046481242bc30a5284598dba6d6f301aaf2d1c
Joe <joana.stutz@zhdk.ch>
2024-11-25 14:26:54 +0100

    Update Typo Project

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,64 @@
-let len = 10;
+let message = "InteractionDesignInteractionDesign";
 let bg;
+let font;
+let waves = [];
+
+function preload() {
+  font = loadFont('assets/fonts/PicNic-Regular.otf');
+}
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  frameRate(60);
+  rectMode(CENTER);
+  bg = color(0);
+  textSize(30);
+  textFont(font);
+}
+
+function draw() {
+  background(bg);
+
+  noFill();
+  strokeWeight(5);
+  stroke(20, 250, 40);
+
+  // Fill array
+  for (let i = 0; i < waves.length; i++) {
+    let wave = waves[i];
+    textWave(wave.offset, wave.angle);
+  }
+}
+
+function textWave(offset, angle) {
+  let currentChar = message[0];
+  let yspace = height / message.length;
+
+  for (let i = 0; i < message.length; i++) {
+    let sinWave = sin(radians(i * 10 + frameCount) + PI * offset);
+    fill(150 + sinWave * 100);
+    textSize(35 + sinWave * 30);
+
+    let x = sinWave * 200 * 0.3;
+    let y = i * yspace;
+
+	let rotatedX = x * cos(angle) - y * sin(angle);
+    let rotatedY = x * sin(angle) + y * cos(angle);
+
+    text(message[i], rotatedX + width / 2, rotatedY + height / 2);
+  }
+}
+
+function mouseClicked() {
+  let newWave = {
+    offset: waves.length, 
+    angle: random(TWO_PI),
+  };
+  waves.push(newWave);
+
+  if (mouseClicked == 10) {
+	waves = [];
 	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+  }
 }
-
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
-	}
-}
-
